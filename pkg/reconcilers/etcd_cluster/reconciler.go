b939010a6751862f34e638f93f212cee3d39c3dc
Chris Werner Rau <cwr@teuto.net>
2026-03-31 15:29:34 +0200

    fix: inverted condition

--- pkg/reconcilers/etcd_cluster/reconciler.go
+++ pkg/reconcilers/etcd_cluster/reconciler.go
@@ -47,6 +47,8 @@ import (
 var (
 	etcdVolumeResizeEvent           = "EtcdVolumeAutoResize"
 	etcdVolumeSizeReCalculatedEvent = "EtcdVolumeSizeRecalculated"
+
+	etcdClientVersion3_7 = semver.MustParse(version.V3_7.String())
 )
 
 type EtcdClusterReconciler interface {
@@ -814,7 +816,7 @@ func (er *etcdClusterReconciler) buildEtcdArgs(
 		"quota-backend-bytes":         strconv.Itoa(int(storageQuota)),
 	}
 
-	if int64(etcdVersion.Minor) >= version.V3_7.Minor { //nolint:gosec // semver minor will never overflow int64
+	if etcdVersion.LT(etcdClientVersion3_7) {
 		// this is deprecated and will be removed in 3.7
 		// TODO: remove this when we roll 3.7
 		args["snapshot-count"] = "10000"
